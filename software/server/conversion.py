a3710cab30602e843a4cae0e614a0f181de81062
Milosz Malczak <milosz.malczak@cern.ch>
2019-04-03 16:53:08 +0200

    server: simplified conversion functions

--- software/server/conversion.py
+++ software/server/conversion.py
@@ -1,17 +1,13 @@
 def threshold_raw_to_mV(threshold, unique_ADC_name, idx, available_ADCs):
     channel_range = available_ADCs[unique_ADC_name].channels[idx].channel_range
-    if(channel_range == 1):
-        threshold = threshold/10
-    elif(channel_range == 100):
-        threshold = threshold/100
+    threshold_mult = {10:1, 1:1/10, 100:1/100}
+    threshold = threshold * threshold_mult[channel_range]
     return int(threshold*10000/2**16)
 
 def threshold_mV_to_raw(threshold, unique_ADC_name, idx, available_ADCs):
     channel_range = available_ADCs[unique_ADC_name].channels[idx].channel_range
-    if(channel_range == 1):
-        threshold = threshold*10
-    elif(channel_range == 100):
-        threshold = threshold*100
+    threshold_mult = {10:1, 1:10, 100:100}
+    threshold = threshold * threshold_mult[channel_range]
     return int(threshold*2**16/10000)
 
 
