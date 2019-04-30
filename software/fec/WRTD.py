3f28857e79461f6d99d65bc7c99035cfda4b182a
Milosz Malczak <milosz.malczak@cern.ch>
2019-04-30 12:07:39 +0200

    adc: Created class WRTD_wrapper

--- software/fec/WRTD.py
+++ software/fec/WRTD.py
@@ -13,32 +13,33 @@ def encode_arguments(func):
         func(self, *args, **kwargs)    
     return wrapper
 
-class WRTD:
+class WRTD(WRTD_wrapper):
     def __init__(self, trtl):
+        super().__init__()
         self.trtl = trtl.encode('utf-8')
         self.wrtd_p = POINTER(wrtd)()
-        status = wrtd_init(self.trtl, 0, None, byref(self.wrtd_p))
-        if(status != WRTD_SUCCESS):
+        status = self.wrtd_init(self.trtl, 0, None, byref(self.wrtd_p))
+        if(status != self.WRTD_SUCCESS):
             print("Cannot open WRTD: " + str(args.D[0]) + " status: 0x" + str(format(status, '08x')) + " errno: " + os.strerror(get_errno()) )
             os._exit(1)
 
-        status = wrtd_disable_all_rules(self.wrtd_p)
+        status = self.wrtd_disable_all_rules(self.wrtd_p)
         self.check_for_errors(status, 'disable_all_rules')
 
-        status = wrtd_remove_all_rules(self.wrtd_p)
+        status = self.wrtd_remove_all_rules(self.wrtd_p)
         self.check_for_errors(status, 'remove_all_rules')
 
     def check_for_errors(self, status, cmd):
         error_code = c_int()
         error_description = create_string_buffer(256)
-        if(status != WRTD_SUCCESS):
-            wrtd_get_error(self.wrtd_p, byref(error_code), 256, error_description)
+        if(status != self.WRTD_SUCCESS):
+            self.wrtd_get_error(self.wrtd_p, byref(error_code), 256, error_description)
             print("Error while executing command: " + str(cmd) + " : " + str(error_description.value))
             os._exit(1)
 
     @encode_arguments
     def add_rule(self, name):
-        status = wrtd_add_rule(self.wrtd_p, name)
+        status = self.wrtd_add_rule(self.wrtd_p, name)
         self.check_for_errors(status, 'add_rule')
        
     @encode_arguments
@@ -48,21 +49,21 @@ class WRTD:
         ts.ns = 0
         ts.frac = 0
         ts_add_ps(ts, delay_ps)
-        status = wrtd_set_attr_string(self.wrtd_p, name, WRTD_ATTR_RULE_SOURCE, src_p)
+        status = self.wrtd_set_attr_string(self.wrtd_p, name, self.WRTD_ATTR_RULE_SOURCE, src_p)
         self.check_for_errors(status, 'set_rule')
-        status = wrtd_set_attr_string(self.wrtd_p, name, WRTD_ATTR_RULE_DESTINATION, dst_p)
+        status = self.wrtd_set_attr_string(self.wrtd_p, name, self.WRTD_ATTR_RULE_DESTINATION, dst_p)
         self.check_for_errors(status, 'set_rule')
-        status = wrtd_set_attr_tstamp(self.wrtd_p, name, WRTD_ATTR_RULE_DELAY, byref(ts))
+        status = self.wrtd_set_attr_tstamp(self.wrtd_p, name, self.WRTD_ATTR_RULE_DELAY, byref(ts))
         self.check_for_errors(status, 'set_rule')
 
     @encode_arguments
     def enable_rule(self, name):
-        status = wrtd_set_attr_bool(self.wrtd_p, name, WRTD_ATTR_RULE_ENABLED, 1);
+        status = self.wrtd_set_attr_bool(self.wrtd_p, name, self.WRTD_ATTR_RULE_ENABLED, 1);
         self.check_for_errors(status, 'enable_rule')
 
     @encode_arguments
     def disable_rule(self, name):
-        status = wrtd_set_attr_bool(self.wrtd_p, name, WRTD_ATTR_RULE_ENABLED, 0);
+        status = self.wrtd_set_attr_bool(self.wrtd_p, name, self.WRTD_ATTR_RULE_ENABLED, 0);
         self.check_for_errors(status, 'disable_rule')
 
     def add_rule_mult_src(self, name, number):
@@ -83,7 +84,7 @@ class WRTD:
 
 
     def __del__(self):
-        wrtd_close(self.wrtd_p)
+        self.wrtd_close(self.wrtd_p)
 
 
 
