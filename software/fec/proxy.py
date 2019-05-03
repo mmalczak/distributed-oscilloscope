646a0447c585ded6eaa926d9de4798c286805945
Milosz Malczak <milosz.malczak@cern.ch>
2019-05-03 21:15:31 +0200

    adc: Working on style

--- software/fec/proxy.py
+++ software/fec/proxy.py
@@ -5,10 +5,7 @@ from xmlrpc.server import SimpleXMLRPCServer
 def get_proxy(addr):
     return xmlrpc.client.ServerProxy(addr)
 
+
 class Proxy():
-   def __init__(self):
-      self.proxy_addr = None
-
-
-
-
+    def __init__(self):
+        self.proxy_addr = None
