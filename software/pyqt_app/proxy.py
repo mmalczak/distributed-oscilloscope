11fc3e36fd0b2fc5ff4733abdb00b2627c62f8c5
Miłosz Malczak <milosz.malczak@cern.ch>
2019-05-04 23:33:01 +0200

    gui: fixed style proxy.py

--- software/pyqt_app/proxy.py
+++ software/pyqt_app/proxy.py
@@ -1,13 +1,11 @@
 import xmlrpc.client
 from xmlrpc.server import SimpleXMLRPCServer
 
-def get_proxy(addr):                     
-    return xmlrpc.client.ServerProxy(addr)  
+
+def get_proxy(addr):
+    return xmlrpc.client.ServerProxy(addr)
+
 
 class Proxy():
-   def __init__(self):
-      self.proxy_addr = None        
-
-
-
-
+    def __init__(self):
+        self.proxy_addr = None
