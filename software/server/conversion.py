f1f25ed4fc8a84dccc7fbd7337f9b3356841fdab
Miłosz Malczak <milosz.malczak@cern.ch>
2019-05-04 19:10:13 +0200

    server: fixed style conversion

--- software/server/conversion.py
+++ software/server/conversion.py
@@ -1,13 +1,16 @@
-def threshold_raw_to_mV(threshold, unique_ADC_name, idx, available_ADCs):
-    channel_range = available_ADCs[unique_ADC_name].channels[idx].channel_range
-    threshold_mult = {10:1, 1:1/10, 100:1/100}
+def threshold_raw_to_mV(threshold, unique_ADC_name, idx,
+                        available_ADCs):
+    channel_range = available_ADCs[unique_ADC_name].channels[idx].\
+                                                        channel_range
+    threshold_mult = {10: 1, 1: 1/10, 100: 1/100}
     threshold = threshold * threshold_mult[channel_range]
     return int(threshold*10000/2**16)
 
-def threshold_mV_to_raw(threshold, unique_ADC_name, idx, available_ADCs):
-    channel_range = available_ADCs[unique_ADC_name].channels[idx].channel_range
-    threshold_mult = {10:1, 1:10, 100:100}
+
+def threshold_mV_to_raw(threshold, unique_ADC_name, idx,
+                        available_ADCs):
+    channel_range = available_ADCs[unique_ADC_name].channels[idx].\
+                                                        channel_range
+    threshold_mult = {10: 1, 1: 10, 100: 100}
     threshold = threshold * threshold_mult[channel_range]
     return int(threshold*2**16/10000)
-
-
