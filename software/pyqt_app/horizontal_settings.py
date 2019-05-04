59ef92e2dc674a18b31b9c4ca24f63103bbe28b7
Miłosz Malczak <milosz.malczak@cern.ch>
2019-05-04 23:18:48 +0200

    gui: fixed style horizontal_settings.py

--- software/pyqt_app/horizontal_settings.py
+++ software/pyqt_app/horizontal_settings.py
@@ -1,32 +1,32 @@
 from parent_classes import *
 from proxy import *
 
-class Presamples(Box):
-   def __init__(self, server_proxy, GUI_name):
-      super().__init__(0, None, "Presamples")
-      self.box.setMinimum(0)
-      self.box.setMaximum(1000000) # TODO check how many pre and postsamples maximum
-      self.server_proxy = server_proxy
-      self.GUI_name = GUI_name
 
-   def value_change(self):
-      presamples = self.box.value()
-      proxy = get_proxy(self.server_proxy.proxy_addr)
-      proxy.set_presamples(presamples, self.GUI_name)
+class Presamples(Box):
+    def __init__(self, server_proxy, GUI_name):
+        super().__init__(0, None, "Presamples")
+        self.box.setMinimum(0)
+        self.box.setMaximum(1000000)
+        """TODO check how many pre and postsamples maximum"""
+        self.server_proxy = server_proxy
+        self.GUI_name = GUI_name
+
+    def value_change(self):
+        presamples = self.box.value()
+        proxy = get_proxy(self.server_proxy.proxy_addr)
+        proxy.set_presamples(presamples, self.GUI_name)
 
 
 class Postsamples(Box):
-   def __init__(self, server_proxy, GUI_name):
-      super().__init__(0, None, "Postsamples")
-      self.box.setMinimum(0)
-      self.box.setMaximum(1000000) # TODO check how many pre and postsamples maximum
-      self.server_proxy = server_proxy
-      self.GUI_name = GUI_name
+    def __init__(self, server_proxy, GUI_name):
+        super().__init__(0, None, "Postsamples")
+        self.box.setMinimum(0)
+        self.box.setMaximum(1000000)
+        """TODO check how many pre and postsamples maximum"""
+        self.server_proxy = server_proxy
+        self.GUI_name = GUI_name
 
-   def value_change(self):
-      postsamples = self.box.value()
-      proxy = get_proxy(self.server_proxy.proxy_addr)
-      proxy.set_postsamples(postsamples, self.GUI_name)
-
-
-
+    def value_change(self):
+        postsamples = self.box.value()
+        proxy = get_proxy(self.server_proxy.proxy_addr)
+        proxy.set_postsamples(postsamples, self.GUI_name)
