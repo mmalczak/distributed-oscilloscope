f1b98d82ea59494721522aa498e4056f8a8b6744
Milosz Malczak <milosz.malczak@cern.ch>
2019-05-17 18:08:58 +0200

    test: moving to appriopriate folder before calling adc_client - otherwise relative paths didn;t work after moving proxy.py to general

--- software/tests/paramiko_connection.py
+++ software/tests/paramiko_connection.py
@@ -15,11 +15,12 @@ def start_adc(port, pci):
     client.load_system_host_keys()
     client.set_missing_host_key_policy(paramiko.AutoAddPolicy())
     client.connect(host, username='root')
-    (stdin, stdout, stderr) = client.exec_command("python3.6 "
-        "/home/Projects/distributed_oscilloscope/software/fec/adc_client.py "
-        "--ip_server 128.141.79.22 --port " +str(port) + " --pci_addr " + 
-        str(pci))
+    command = "cd /home/Projects/distributed_oscilloscope/software/fec/;" +\
+        "python3.6 adc_client.py --ip_server 128.141.79.22 --port " +\
+         str(port) + " --pci_addr " + str(pci)
+    (stdin, stdout, stderr) = client.exec_command(command)
     #(stdin, stdout, stderr) = client.exec_command("ps -A | grep python")
     #for line in stdout.readlines():
-    #    print(line.split()[0])
+        #print(line.split()[0])
+    #    print(line)
 
