de45dce47e72b49fdd68e8874ff33aa011c14763
Milosz Malczak <milosz.malczak@cern.ch>
2019-05-21 11:09:27 +0200

    test: changed sleep time from 1s to 0.1s

--- software/tests/test.py
+++ software/tests/test.py
@@ -25,28 +25,28 @@ class OscilloscopeMethods(unittest.TestCase):
         self.server_handler = subprocess.Popen(
                         'python3 ../server/main_server.py', shell=True)
         self.ADCs = {'ADC1':[8000, 1], 'ADC2':[8001, 2]}
-        time.sleep(1)
+        time.sleep(0.1)
         ADC1 = self.ADCs['ADC1']
         start_adc(ADC1[0], ADC1[1])
-        time.sleep(1)
+        time.sleep(0.1)
         ADC2 = self.ADCs['ADC2']
         start_adc(ADC2[0], ADC2[1])
-        time.sleep(1)
+        time.sleep(0.1)
         self.server_expose = ThreadServerExposeTest(None, 8001) 
-        time.sleep(1)
+        time.sleep(0.1)
 
     def __del__(self, *args, **kwargs):
         super(OscilloscopeMethods, self).__init__(*args, **kwargs)
-        time.sleep(1)
+        time.sleep(0.1)
         self.remove_ADC('ADC1')
-        time.sleep(1)
+        time.sleep(0.1)
         self.remove_ADC('ADC2')
-        time.sleep(1)
+        time.sleep(0.1)
         self.server_handler.terminate()
         self.server_expose.thread.terminate()
 
     def test_upper(self):
-        time.sleep(1)
+        time.sleep(0.1)
         self.assertEqual("abc", "abc")
        
 
