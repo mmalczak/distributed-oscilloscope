76022db0d969aaac78274a013dfbd4d3d47c8ea8
Milosz Malczak <milosz.malczak@cern.ch>
2019-05-29 10:44:39 +0200

    gui: for now when connecting to the server, it's address is hardcoded

--- software/pyqt_app/zmq_rpc.py
+++ software/pyqt_app/zmq_rpc.py
@@ -12,7 +12,8 @@ class ZMQ_RPC():
     def __init__(self):
         context = zmq.Context()
         self.socket = context.socket(zmq.REQ)
-        self.socket.connect("tcp://localhost:8003")
+#        self.socket.connect("tcp://localhost:8003")
+        self.socket.connect("tcp://128.141.79.50:8003")
 
     #def set_GUI_parameter(self, name, value, GUI_name):
     def send_RPC(self, function_name, *args):
