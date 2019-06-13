ba7196ea95f5e78a2230d66778b1f0d94b607e42
Milosz Malczak <milosz.malczak@cern.ch>
2019-06-13 11:55:56 +0200

    adc: removed old unused XMLRPC code

--- software/fec/server_expose.py
+++ software/fec/server_expose.py
@@ -48,25 +48,6 @@ class ServerExpose():
         os._exit(1)
 
     def run(self):
-        self.server = SimpleXMLRPCServer((self.addr, self.port),
-                                         allow_none=True)
-        self.server.register_function(self.set_adc_parameter,
-                                      "set_adc_parameter")
-        self.server.register_function(self.set_server_address,
-                                      "set_server_address")
-        self.server.register_function(self.adc.get_current_conf,
-                                      "get_current_conf")
-        self.server.register_function(self.adc.
-                configure_acquisition_retrieve_and_send_data,
-                "configure_acquisition_async")
-        self.server.register_function(self.adc.set_WRTD_master,
-                                      "set_WRTD_master")
-        self.server.register_function(self.adc.stop_acquisition,
-                                      "stop_acquisition")
-        self.server.register_function(self.exit, "exit")
-
-
-        """self.server.serve_forever()"""
 
         context = zmq.Context()
         socket = context.socket(zmq.ROUTER)
@@ -85,8 +66,6 @@ class ServerExpose():
             if name.startswith('EVENT_'):
                 value = getattr(zmq, name)
                 EVENT_MAP[value] = name
-
-
 
         self.adc.selector = poller
         while True:
