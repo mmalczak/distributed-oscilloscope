f7f441c0518a3a4414069c04d681df717d755238
Milosz Malczak <milosz.malczak@cern.ch>
2019-06-13 13:35:28 +0200

    adc: removed enum comments from the wrappers

--- software/fec/wrtd_wrapper.py
+++ software/fec/wrtd_wrapper.py
@@ -55,11 +55,6 @@ def encode_arguments(func):
 
 
 class WRTD_wrapper():
-    """@enum wrtd_status
-        White Rabbit Trigger Distribution warnings and errors
-
-        enum wrtd_status {
-             Codes inspired by IVI 3.2"""
     WRTD_SUCCESS = 0
     WRTD_ERROR_BASE = 0xBFFA3000
     WRTD_ERROR_CANNOT_RECOVE = 0xBFFA3001
@@ -79,30 +74,20 @@ class WRTD_wrapper():
     WRTD_ERROR_RESET_FAILED = 0xBFFA300F
     WRTD_ERROR_RESOURCE_UNKNOWN = 0xBFFA3010
 
-    """Resource (alar, rule) is not disabled and cannot be changed"""
     WRTD_ERROR_RESOURCE_ACTIVE = 0xBFFA3011
 
-    """Require global rep cap"""
     WRTD_ERROR_ATTR_INVALID_REP_CAP = 0xBFFA3012
 
-    """Feature not implemented"""
     WRTD_ERROR_NOT_IMPLEMENTED = 0xBFFA3013
 
-    """Incorrect repeated capability id: too long,
-       invalid character..."""
     WRTD_ERROR_BAD_REP_CAP_ID = 0xBFFA3014
 
-    """Timeout while reading log"""
     WRTD_ERROR_TIMEOUT = 0xBFFA3015
 
-    """Output buffer is not long enough."""
     WRTD_ERROR_BUFFER_TOO_SHORT = 0xBFFA3016
 
     WRTD_ERROR_UNKNOWN_NAME_IN_SELECTOR = 0xBFFA3017
-    """NOTE: selector == rep_cap_id"""
 
-    """Codes inspired by IviLxiSync
-       3.15"""
     WRTD_ERROR_ALARM_TIME_INVALID = 0xBFFA3018
     WRTD_ERROR_ALARM_EXISTS = 0xBFFA3019
     WRTD_ERROR_ALARM_DOES_NOT_EXIST = 0xBFFA301A
@@ -115,55 +100,31 @@ class WRTD_wrapper():
     WRTD_ERROR_CANT_REMOVE_RESERVED_REP_CAP = 0xBFFA3021
     __WRTD_ERROR_MAX_NUMBER = 0xBFFA3022
 
-    """@enum wrtd_attr
-      White Rabbit Trigger Distribution attribute ID definions"""
 
-    """enum wrtd_attr"""
     __WRTD_ATTR_BASE = 950000
     WRTD_MAJOR_VERSION = 950001
     WRTD_MINOR_VERSION = 950002
     WRTD_ATTR_EVENT_LOG_ENTRY_COUNT = 950003
     WRTD_ATTR_EVENT_LOG_ENABLED = 950004
-    """TODO: add log levelsmasks here"""
     WRTD_ATTR_IS_TIME_MASTER = 950005
     WRTD_ATTR_IS_TIME_SYNCHRONIZED = 950006
-    """Number of alarms (global attribute)"""
     WRTD_ATTR_ALARM_COUNT = 950007
-    """Enabledisable an alarm"""
     WRTD_ATTR_ALARM_ENABLED = 950008
     WRTD_ATTR_ALARM_SETUP_TIME = 950009
     WRTD_ATTR_ALARM_TIME = 950010
-    """Specifies the alarm period. 0 means no repetitions."""
     WRTD_ATTR_ALARM_PERIOD = 950011
-    """Specifies the number of times the alarm will occur at the period
-       specified by WRTD_ATTR_ALARM_PERIOD. 0 means infinite. 1 means
-       exactly one alarm will occur"""
     WRTD_ATTR_ALARM_REPEAT_COUNT = 950012
-    """Number of rules (global attribute)."""
     WRTD_ATTR_RULE_COUNT = 950013
-    """True if the rule is enabled (ie active)"""
     WRTD_ATTR_RULE_ENABLED = 950014
-    """Number of time the rule will apply.  0 means infinite.
-       When read, return the remaining number"""
     WRTD_ATTR_RULE_REPEAT_COUNT = 950015
-    """Source of the event."""
     WRTD_ATTR_RULE_SOURCE = 950016
-    """Destination of the event."""
     WRTD_ATTR_RULE_DESTINATION = 950017
-    """If true, events from the past are still handled; otherwise they
-       are discarded"""
     WRTD_ATTR_RULE_SEND_LATE = 950018
-    """Delay the event."""
     WRTD_ATTR_RULE_DELAY = 950019
-    """Discard new events between the last one to this value"""
     WRTD_ATTR_RULE_HOLDOFF = 950020
-    """Realign the timestamp to a multiple of period and then add factor
-       period. This is done after the delay"""
     WRTD_ATTR_RULE_RESYNC_PERIOD = 950021
     WRTD_ATTR_RULE_RESYNC_FACTOR = 950022
 
-    """TODO: add __ boundaries between groups to make it easier to do
-       input validation"""
     WRTD_ATTR_STAT_RULE_RX_EVENTS = 950023
     WRTD_ATTR_STAT_RULE_RX_LAST = 950024
     WRTD_ATTR_STAT_RULE_TX_EVENTS = 950025
@@ -173,7 +134,6 @@ class WRTD_wrapper():
     WRTD_ATTR_STAT_RULE_MISSED_EVENTS_OVERFLOW = 950029
     WRTD_ATTR_STAT_RULE_MISSED_LAST = 950030
 
-    """Latency and period (excluding events discarded due to holdoff)"""
     WRTD_ATTR_STAT_RULE_RX_LATENCY_MIN = 950031
     WRTD_ATTR_STAT_RULE_RX_LATENCY_MAX = 950032
     WRTD_ATTR_STAT_RULE_RX_LATENCY_AVG = 950033
@@ -182,14 +142,9 @@ class WRTD_wrapper():
     WRTD_ATTR_STAT_RULE_RX_PERIOD_AVG = 950036
     __WRTD_ATTR_MAX_NUMBER = 950037
 
-    """A repeated capability identifier for global attributes"""
     WRTD_GLOBAL_REP_CAP_ID = 'WGRCI'
 
-    """@enum wrtd_log_type
-       White Rabbit Trigger Distribution log entry type"""
 
-    """enum wrtd_log_type {
-             TODO: add log types"""
     __WRTD_MAX_LOG_TYPE_NUMBER = 0
 
     def __init_lib(self):
