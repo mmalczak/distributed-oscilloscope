04f7eef870bbe06e7ce4cfd0c2ba4d91278c828c
Milosz Malczak <milosz.malczak@cern.ch>
2019-06-20 15:27:05 +0200

    server: added IP pblisher

--- software/server/publisher.py
+++ software/server/publisher.py
@@ -5,12 +5,26 @@ logger = logging.getLogger(__name__)
 
 
 class Publisher():
-    """TODO should I add some logging here or some error handling?"""
+
     def __init__(self, ip, port):
+        """TODO should I add some logging here or some error handling?"""
         context = zmq.Context()
         self.socket = context.socket(zmq.DEALER)
         addr = str(ip) + ':' + str(port)
         self.socket.connect("tcp://" + addr)
+
+    def send_message(self, data):
+        msg = pickle.dumps(data)
+        self.socket.send(msg)
+
+
+class PublisherIPC():
+
+    def __init__(self, ipc_name):
+        """TODO should I add some logging here or some error handling?"""
+        context = zmq.Context()
+        self.socket = context.socket(zmq.DEALER)
+        self.socket.connect("ipc:///tmp/" + ipc_name)
 
     def send_message(self, data):
         msg = pickle.dumps(data)
