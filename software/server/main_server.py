f691373d8d6031106895c18da7623a3790876c0e
Milosz Malczak <milosz.malczak@cern.ch>
2019-06-20 16:05:57 +0200

    server: renamed service_management to zeroconf_listener

--- software/server/main_server.py
+++ software/server/main_server.py
@@ -1,6 +1,6 @@
 import sys
 from oscilloscope import Oscilloscope
-from service_management import ThreadZeroConf
+from zeroconf_listener import ThreadZeroConf
 from expose import Expose
 import logging.config
 from logging_conf import DEFAULT_CONFIG
