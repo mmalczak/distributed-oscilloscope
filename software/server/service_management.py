2e1a34ec661400f4c0879104572d37a00ef02c88
Milosz Malczak <milosz.malczak@cern.ch>
2019-06-20 10:15:55 +0200

    server: removed functions add/remove service

--- software/server/service_management.py
+++ software/server/service_management.py
@@ -22,42 +22,24 @@ class ZeroconfListener:
         self.osc = osc
 
     def remove_service(self, zeroconf, type, name):
-        remove_service(name, self.osc)
+        if name.startswith("ADC"):
+            self.osc.unregister_ADC(name)
 
     def add_service(self, zeroconf, type, name):
-        info = zeroconf.get_service_info(type, name)
-        conf = None
-        if b'conf' in info.properties:
-            conf = info.properties[b'conf']
-        try:
-            add_service(name, socket.inet_ntoa(info.address),
-                        str(int(info.properties[b'port'])), self.osc, conf)
-        except Exception as e:
-            pass
-
-
-def remove_service(name, osc):
-    """adding and removing service could be done either by zeroconf,
-    or directly by the client using RPC call"""
-
-    if name.startswith("ADC"):
-        print("Service %s removed" % (name,))
-        osc.unregister_ADC(name)
-
-
-def add_service(name, addr, port, osc, conf=None, server_addr_known=False):
-
-    if name.startswith("ADC"):
-        """name provided by zeroconf ihas to be unique and is not user
-        friendly, thats why user selected name is passed in properties"""
-        osc.register_ADC(name, conf['board_conf']['n_chan'], str(addr), port)
-        """server_address should be set at the end, (ADC checks if
-        server_address was set and assumes that if it was set, server
-        is properly configured)"""
-        if not server_addr_known:
-            server_addr = os.popen("ifconfig| grep inet").read().split()[1]
-            ADC = self.osc.get_ADC(unique_ADC_name)
-            zmq_rpc = ADC.zmq_rpc
-            zmq_rpc.send_RPC('set_server_address', server_addr)
-            """This part is not tested"""
-        print("Service %s added" % (name))
+        if name.startswith("ADC"):
+            info = zeroconf.get_service_info(type, name)
+            conf = None
+            if b'conf' in info.properties:
+                conf = info.properties[b'conf']
+            try:
+                addr = socket.inet_ntoa(info.address)
+                port = str(int(info.properties[b'port']))
+                osc.register_ADC(name, conf['board_conf']['n_chan'], str(addr), port)
+                if not server_addr_known:
+                    server_addr = os.popen("ifconfig| grep inet").read().split()[1]
+                    ADC = self.osc.get_ADC(unique_ADC_name)
+                    zmq_rpc = ADC.zmq_rpc
+                    zmq_rpc.send_RPC('set_server_address', server_addr)
+                    """This part is not tested"""
+            except Exception as e:
+                pass
