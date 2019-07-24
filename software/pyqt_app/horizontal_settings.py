4f7e335db6b2167a2745516f0bb3e13833ad9751
Milosz Malczak <milosz.malczak@cern.ch>
2019-07-24 10:41:45 +0200

    gui: exchanged the presamples and postsamples to time and percentage

--- software/pyqt_app/horizontal_settings.py
+++ software/pyqt_app/horizontal_settings.py
@@ -4,12 +4,11 @@ sys.path.append('../')
 from general.zmq_rpc import *
 from PyQt5.QtWidgets import QHBoxLayout
 
-
-class Presamples(Box):
+class Percentage(Box):
     def __init__(self, zmq_rpc, GUI_name, GUI, acq_set):
-        super().__init__(0, "No_ADC", "Presamples")
+        super().__init__(0, "No_ADC", "Percentage")
         self.box.setMinimum(0)
-        self.box.setMaximum(1000000)
+        self.box.setMaximum(99)
         """TODO check how many pre and postsamples maximum"""
         self.zmq_rpc = zmq_rpc
         self.GUI_name = GUI_name
@@ -17,14 +16,15 @@ class Presamples(Box):
         self.acq_set = acq_set
 
     def value_change(self):
-        presamples = self.box.value()
-        self.acq_set.set_pre_post_samples(presamples=presamples)
+        percentage = self.box.value()
+        self.acq_set.set_acq_set(percentage=percentage)
 
-class Postsamples(Box):
+
+class Time(Box):
     def __init__(self, zmq_rpc, GUI_name, GUI, acq_set):
-        super().__init__(0, "No_ADC", "Postsamples")
-        self.box.setMinimum(0)
-        self.box.setMaximum(1000000)
+        super().__init__(0, "No_ADC", "Acq Time[us]")
+        self.box.setMinimum(1)
+        self.box.setMaximum(1000)
         """TODO check how many pre and postsamples maximum"""
         self.zmq_rpc = zmq_rpc
         self.GUI_name = GUI_name
@@ -32,8 +32,9 @@ class Postsamples(Box):
         self.acq_set = acq_set
 
     def value_change(self):
-        postsamples = self.box.value()
-        self.acq_set.set_pre_post_samples(postsamples=postsamples)
+        time = self.box.value()
+        self.acq_set.set_acq_set(time=time)
+
 
 class AcquisitionSettings(QHBoxLayout):
     def __init__(self, zmq_rpc, GUI_name, GUI):
@@ -41,20 +42,25 @@ class AcquisitionSettings(QHBoxLayout):
         self.zmq_rpc = zmq_rpc
         self.GUI = GUI
         self.GUI_name = GUI_name
-        self.presamples = Presamples(zmq_rpc, GUI_name, GUI, self)
-        self.postsamples = Postsamples(zmq_rpc, GUI_name, GUI, self)
-        self.addWidget(self.presamples)
-        self.addWidget(self.postsamples)
+        self.time= Time(zmq_rpc, GUI_name, GUI, self)
+        self.percentage= Percentage(zmq_rpc, GUI_name, GUI, self)
+        self.addWidget(self.time)
+        self.addWidget(self.percentage)
 
     def set_params(self, presamples, postsamples):
-        self.presamples.set_value(presamples)
-        self.postsamples.set_value(postsamples)
+        time = (presamples + postsamples)/100
+        percentage = presamples / (presamples + postsamples) * 100
+        self.time.set_value(time)
+        self.percentage.set_value(percentage)
 
-    def set_pre_post_samples(self, presamples=None, postsamples=None):
-        if not presamples:
-            presamples = self.presamples.get_value()
-        if not postsamples:
-            postsamples = self.postsamples.get_value()
+    def set_acq_set(self, time=None, percentage=None):
+        if not time:
+            time = self.time.get_value()
+        if not percentage:
+            percentage= self.percentage.get_value()
+        samples = time * 100
+        presamples = int(samples * percentage / 100)
+        postsamples = int(samples * (100 - percentage) / 100)
 
         self.zmq_rpc.send_RPC('set_pre_post_samples', presamples, postsamples,
                               self.GUI_name)
