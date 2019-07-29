4317183cbdbf63bab72a3473bc0b653c53fa8d63
Milosz Malczak <milosz.malczak@cern.ch>
2019-07-29 11:56:07 +0200

    test: changed default configuration of the tests

--- software/applications/tests/test_conf.py
+++ software/applications/tests/test_conf.py
@@ -4,7 +4,7 @@ update_data_types = {1: 'plot',
                      2: 'time_measurements',
                      3: 'frequency_measurements',
                      4: 'precision'}
-update_data_select = 4
+update_data_select = 1
 
 update_data_type = update_data_types[update_data_select]
 
