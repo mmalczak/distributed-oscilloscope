8b998a274929add8148190605eedf852c9b3d7b2
Milosz Malczak <milosz.malczak@cern.ch>
2019-08-19 15:12:47 +0200

    docs: updated conf.py and .readthedocs to build autodoc

--- docs/conf.py
+++ docs/conf.py
@@ -16,7 +16,7 @@
 # add these directories to sys.path here. If the directory is relative to the
 # documentation root, use os.path.abspath to make it absolute, like shown here.
 
-soft_dir = '/home/milosz/Projects/distributed_oscilloscope/software/DistributedOscilloscope/'
+soft_dir = '../software/DistributedOscilloscope/'
 
 import os
 import sys
